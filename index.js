349092c21d958db71ca25be3a683eb4b9693ad8c
Alextryfg <Alextryfg>
2025-05-22 18:15:42 +0200

    fix: favourite languague

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/api/hello', function (req, res) {
 
 app.get("/api/whoami", (req,res) => {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  const language = req.headers['accept-language'];
+  const language = req.headers['favorite-language'];
   const software = req.headers['user-agent'];
   res.json({
     ipaddress: ip,
