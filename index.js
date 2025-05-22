f5e939d22b95c095b2642675d658e8e735753c64
Alextryfg <Alextryfg>
2025-05-22 18:17:20 +0200

    fix

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ app.get('/api/hello', function (req, res) {
 
 app.get("/api/whoami", (req,res) => {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  const language = req.headers['favorite-language'];
+  const preferredLanguage = req.headers['accept-language']?.split(',')[0];
   const software = req.headers['user-agent'];
   res.json({
     ipaddress: ip,
-    languague: language,
+    languague: preferredLanguage,
     software: software
   })
 })
