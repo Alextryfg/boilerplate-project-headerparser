9e03e1919ebff746599158bdd5d1fc8523c35f6e
Alextryfg <Alextryfg>
2025-06-01 14:10:19 +0200

    done

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); 
+
+
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -35,7 +34,7 @@ app.get("/api/whoami", (req,res) => {
   })
 })
 
-// listen for requests :)
+
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
